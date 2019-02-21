80a49b80ff04af0df3c924217c5c3a751424b083
TB831 <druu831@gmail.com>
2019-02-21 06:02:27 -0800

    attack implemented

--- app.js
+++ app.js
@@ -12,15 +12,35 @@ new Vue({
             this.monsterHealth = 100;
         },
         attack: function() {    // Method for attacking
+            let max = 10;
+            let min = 3;
+            let damage = Math.max(Math.floor(Math.random() * max) + 1, min); // Generates random num between 3-10
+            this.monsterHealth -= damage;
+
+            if (this.monsterHealth <= 0) {
+                alert('You Won!');
+                this.gameIsRunning = false;
+                return;
+            }
+
+            max = 12;
+            min = 5;
+            damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+            this.playerHealth -= damage;
+
+            if (this.playerHealth <= 0) {
+                alert('You Lost!');
+                this.gameIsRunning = false;
+                return;
+            }
+        },
+        specialAttack: function() { // Method for special attack
 
         },
-        specialAttack: function() {
+        heal: function() {  // Method for healthing player
 
         },
-        heal: function() {
-
-        },
-        giveUp: function() {
+        giveUp: function() {    // Method for starting new game
 
         },
     }
